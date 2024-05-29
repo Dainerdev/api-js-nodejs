2b5c2b2491a93fd2641076552be147749b9ece8a
Dainerdev <florianarroyo15@gmail.com>
2024-05-28 21:46:00 -0500

    Lectura de json

--- src/app.js
+++ src/app.js
@@ -11,8 +11,9 @@ app.set("port", 3312);
 
 // Middelwares (funciones entre una peticion y una respuesta)
 app.use(morgan("dev"));
+app.use(express.json());
 
 // Routes
-app.use("/api/experiences",experiencesRoutes);
+app.use("/api/experiences", experiencesRoutes);
 
 export default app;
