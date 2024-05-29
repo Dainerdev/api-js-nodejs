89e50fefe1948e771eb22d4171f1798a34426eda
Dainerdev <florianarroyo15@gmail.com>
2024-05-28 21:47:02 -0500

    metodo get realizado

--- src/controllers/experiences.controller.js
+++ src/controllers/experiences.controller.js
@@ -1,7 +1,29 @@
-const getExperiences= (req, res) => {
-    res.json("SI, ESTAMOS VIVOS");
+import { getConnection } from "./../database/database";
+
+const getExperiences = async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT id, empresa, puesto, fecha_inicio, fecha_fin, responsabilidades, salario FROM experiences");
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }    
+};
+
+const addExperiences = async (req, res) => {
+    try {
+        console.log(req.body);
+        const connection = await getConnection();
+
+        res.json(addExperience);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }    
 };
 
 export const methods = {
-    getExperiences
+    getExperiences,
+    addExperiences
 };
--- src/routes/experiences.route.js
+++ src/routes/experiences.route.js
@@ -4,5 +4,6 @@ import { methods as experiencesController } from "../controllers/experiences.con
 const router = Router();
 
 router.get("/", experiencesController.getExperiences);
+router.post("/", experiencesController.addExperiences);
 
 export default router;
