77f2d181f275b2c5d9387fbe4fb0beef6ab563ad
Dainerdev <florianarroyo15@gmail.com>
2024-05-28 20:25:18 -0500

    Configuracion de archivos.

--- src/app.js
+++ src/app.js
@@ -7,5 +7,6 @@ const app = express();
 app.set("port", 3321);
 
 // Middelwares (funciones entre una peticion y una respuesta)
+app.use(morgan("dev"));
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-const express = require('express');
-const morgan = require('morgan');
-const pool = require('./db_connection');
-
-const app = express();
-
-// Settings
-app.set('port', process.env.PORT || 3312); // validar si hay puerto default o no
-app.set('json spaces', 2);
-
-// Middlewares
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));// Enivar y recibir archivos json
-app.use(express.json());
-
-// Routes
-// app.use(require('./routes/index'));
-// app.use('/api/movies', require('./routes/movies'));
-// app.use('/api/users', require('./routes/users'));
-app.use('/api/experiences', require('./routes/experiences'));
-
-// Starting server
-app.listen(app.get('port'), () => {
-    console.log(`Server on port http://localhost:${app.get('port')}`);
-});
+import app from "./app";
 
 const main=() => {
-    
+    app.listen(app.get("port"));
+    console.log(`Server on port http://localhost:${app.get("port")}`);
 } 
+
+main();
