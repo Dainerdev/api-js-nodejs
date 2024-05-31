47448166949c7ec81338be58a01c433eb867b880
Dainerdev <florianarroyo15@gmail.com>
2024-05-31 16:16:11 -0500

    ajuste

--- src/routes/experiences.route.js
+++ src/routes/experiences.route.js
@@ -3,10 +3,17 @@ import { methods as experiencesController } from "../controllers/experiences.con
 
 const router = Router();
 
+// Routes
+
+// Find All
 router.get("/", experiencesController.getExperiences);
+// Create
 router.post("/", experiencesController.addExperiences);
+// Finfd By Id
 router.get("/:id", experiencesController.getExperience);
+// Delete
 router.delete("/:id", experiencesController.deleteExperience);
+// Edit
 router.put("/:id", experiencesController.updateExperience);
 
 export default router;
